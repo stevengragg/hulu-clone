83105a7614ccce0d05a436b937570c2cec143414
stevengragg <sgragg5619@gmail.com>
2021-07-26 00:21:22 +0800

    changes: added flip move and other final touches

--- components/Content.js
+++ components/Content.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Thumbnail from './Thumbnail'
+import FlipMove from 'react-flip-move'
 
 function Content({ results }) {
     return (
-        <div>
+        <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex 3xl:flex-wrap 3xl:justify-center">
             {results.map((result) => (
                 <Thumbnail key={result.id} result={result} />
-            ))
-            }
-        </div>
+            ))}
+        </FlipMove>
     )
 }
 
--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -3,11 +3,13 @@ import Image from 'next/image'
 import {
     ThumbUpIcon
 } from '@heroicons/react/outline'
+import { forwardRef } from 'react'
 
-function Thumbnail({ result }) {
+
+const Thumbnail = forwardRef(({ result }, ref) => {
     const BASE_URL = "https://image.tmdb.org/t/p/original/";
     return (
-        <div className="group cursor-pointer p-2 transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
+        <div ref={ref} className="group cursor-pointer p-2 transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
             <Image 
                 layout='responsive'
                 src={`${BASE_URL}${result.backdrop_path || result.poster_path }` || `${BASE_URL}${result.backdrop_path}`}
@@ -27,6 +29,6 @@ function Thumbnail({ result }) {
             </div>
         </div>
     )
-}
+})
 
 export default Thumbnail
